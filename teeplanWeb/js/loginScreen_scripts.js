ce4cc96c7c569d2eacaaaec08039701b2bfa3cfd
AntoniIwan <117358535+AntoniIwan@users.noreply.github.com>
2024-04-27 21:50:19 +0200

    stworzenie przestrzeni do rozwijania aplikacji internetowej

---
+++ teeplanWeb/js/loginScreen_scripts.js
@@ -0,0 +1,15 @@
+
+function checkCredentials() {
+
+        // Pobierz wartości wpisane przez użytkownika
+        var email = document.getElementById("email_input").value;
+        var password = document.getElementById("password_input").value;
+        
+        //Dodać walidacje z bazą danych
+
+     if (email === "123" && password === "123") {
+        window.location.href = "/src/test.html"; //można zmienić na kolejną stronę
+    } else {
+        alert("Nieprawidłowy email lub hasło. Spróbuj ponownie.");
+    }
+}
