1a65993bac91dd31700822caf24750104d591551
masloorzech <antoniiwan02@gmail.com>
2024-04-28 21:15:46 +0200

    Dodanie mozliwosi wylogowania

--- WebApplication/js/loginScreen_scripts.js
+++ WebApplication/js/loginScreen_scripts.js
@@ -8,7 +8,7 @@ function checkCredentials() {
         //Dodać walidacje z bazą danych
 
      if (email === "123" && password === "123") {
-        window.location.href = "/WebApplication/screenAfterLogin.html"; //można zmienić na kolejną stronę
+        window.location.href = "/WebApplication/screenAfterLogin.html";
     } else {
         alert("Nieprawidłowy email lub hasło. Spróbuj ponownie.");
     }
---
+++ WebApplication/js/screenAfterLogin_scripts.js
@@ -0,0 +1,4 @@
+
+function backToLogin(){
+    window.location.href = "/WebApplication/loginScreen.html"; //można zmienić na kolejną stronę
+}
