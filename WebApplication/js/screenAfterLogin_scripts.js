020dde704b3b8f9f811cda16baf9a348328d0c25
AntoniIwan <245819@edu.p.lodz.pl>
2024-05-01 12:18:32 +0200

    Dodanie Wysukiwania i dodawnia kuponów i eventów (Tylko opcje wyboru moliwości bez konfiguracji)

--- WebApplication/js/screenAfterLogin_scripts.js
+++ WebApplication/js/screenAfterLogin_scripts.js
@@ -1,4 +1,47 @@
 
 function backToLogin(){
-    window.location.href = "/WebApplication/loginScreen.html"; //można zmienić na kolejną stronę
+    window.location.href = "/WebApplication/loginScreen.html"; 
+}
+function showOptionsEvents() {
+    var middlePanel = document.getElementById("middlePanel");
+    middlePanel.innerHTML = `<div id="section_panel">
+    <div onclick="goToAddEvents()" id="option_add">
+        <div id="add_text">ADD</div>
+        <div id="add_icon"></div>
+    </div>
+        <div onclick="goToSearchEvents()" id="option_search">
+            <div id="search_text">FIND</div>
+            <div id="search_icon"></div>
+        </div>
+</div>`;
+}
+
+function goToSearchEvents() {
+    window.location.href = "/WebApplication/searchEvents.html"; // Adres strony z wyszukiwaniem
+}
+
+function goToAddEvents() {
+    window.location.href = "/WebApplication/addEvents.html"; // Adres strony z dodawaniem
+}
+function showOptionsCoupon() {
+    var middlePanel = document.getElementById("middlePanel");
+    middlePanel.innerHTML = ` <div id="section_panel">
+    <div onclick="goToAddCoupon()" id="option_add">
+        <div id="add_text">ADD</div>
+        <div id="add_icon"></div>
+    </div>
+        <div onclick="goToSearchCoupon()" id="option_search">
+            <div id="search_text">FIND</div>
+            <div id="search_icon"></div>
+        </div>
+</div>`;
+
+}
+
+function goToSearchCoupon() {
+    window.location.href = "/WebApplication/searchCoupon.html"; // Adres strony z wyszukiwaniem
+}
+
+function goToAddCoupon() {
+    window.location.href = "/WebApplication/addCoupon.html"; // Adres strony z dodawaniem
 }
