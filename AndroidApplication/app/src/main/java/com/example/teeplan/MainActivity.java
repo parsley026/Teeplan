2830e48f7988405305e5a736174221de58c40747
erienx <mat21mat12mat@gmail.com>
2024-05-08 18:09:11 +0200

    Implementacja navigation bar

--- AndroidApplication/app/src/main/java/com/example/teeplan/MainActivity.java
+++ AndroidApplication/app/src/main/java/com/example/teeplan/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.teeplan;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.teeplan.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragemnt(new HomeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int id = item.getItemId();
+            if (id == R.id.home) {
+                replaceFragemnt(new HomeFragment());
+            } else if (id == R.id.todo) {
+                replaceFragemnt(new TodoFragment());
+
+            } else if (id == R.id.timer) {
+                replaceFragemnt(new TimerFragment());
+            } else if (id == R.id.coupon) {
+                replaceFragemnt(new CouponFragment());
+            } else if (id == R.id.settings) {
+                replaceFragemnt(new SettingsFragment());
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragemnt(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
