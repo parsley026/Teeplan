f8357caf8192ea9b1f13ed05a9454e95f1d5b885
fxlipton <114811833+fxlipton@users.noreply.github.com>
2024-06-10 19:20:15 +0200

    Naprawa bledu - zmiana ekranu przy obroceniu telefonu

--- AndroidApplication/app/src/main/java/com/example/teeplan/MainActivity.java
+++ AndroidApplication/app/src/main/java/com/example/teeplan/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.teeplan;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -12,7 +11,6 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
 import com.example.teeplan.coupon.CouponFragment;
 import com.example.teeplan.databinding.ActivityMainBinding;
 import com.example.teeplan.event.EventFragment;
@@ -20,6 +18,8 @@ import com.example.teeplan.event.EventFragment;
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     private static final String TIMER_RUNNING_KEY = "com.example.teeplan.buttonPref";
+    private static final String CURRENT_FRAGMENT_KEY = "CURRENT_FRAGMENT_KEY";
+    private String currentFragmentTag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,35 +36,57 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        replaceFragemnt(new ToDoFragment());
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
+        if (savedInstanceState == null) {
+            replaceFragment(new ToDoFragment(), ToDoFragment.class.getSimpleName());
+        } else {
+            currentFragmentTag = savedInstanceState.getString(CURRENT_FRAGMENT_KEY);
+            Fragment currentFragment = getSupportFragmentManager().findFragmentByTag(currentFragmentTag);
+            if (currentFragment != null) {
+                replaceFragment(currentFragment, currentFragmentTag);
+            }
+        }
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             int id = item.getItemId();
             if (id == R.id.todo) {
-                replaceFragemnt(new ToDoFragment());
+                replaceFragment(new ToDoFragment(), ToDoFragment.class.getSimpleName());
             } else if (id == R.id.events) {
-                replaceFragemnt(new EventFragment());
+                replaceFragment(new EventFragment(), EventFragment.class.getSimpleName());
             } else if (id == R.id.timer) {
-                replaceFragemnt(new TimerFragment());
+                replaceFragment(new TimerFragment(), TimerFragment.class.getSimpleName());
             } else if (id == R.id.coupon) {
-                replaceFragemnt(new CouponFragment());
+                replaceFragment(new CouponFragment(), CouponFragment.class.getSimpleName());
             } else if (id == R.id.settings) {
-                replaceFragemnt(new SettingsFragment());
+                replaceFragment(new SettingsFragment(), SettingsFragment.class.getSimpleName());
             }
 
             return true;
         });
     }
 
-    private void replaceFragemnt(Fragment fragment) {
+    private void replaceFragment(Fragment fragment, String tag) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.replace(R.id.frame_layout, fragment, tag);
         fragmentTransaction.commit();
+        currentFragmentTag = tag;
     }
 
     private void resetTimerToOriginalState() {
         SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
         sharedPreferences.edit().putBoolean(TIMER_RUNNING_KEY, false).apply();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(CURRENT_FRAGMENT_KEY, currentFragmentTag);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        currentFragmentTag = savedInstanceState.getString(CURRENT_FRAGMENT_KEY);
     }
 }
